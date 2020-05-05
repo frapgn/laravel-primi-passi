703ad70c553f6fe23c0307fc97a34b24909155a6
frapgn <59645305+frapgn@users.noreply.github.com>
2020-05-05 12:10:05 +0200

    Divisione in files

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <div id="logo">
+        <img src="{{asset('img/marchio-sito-test.png')}}" alt="">
+        <ul>
+            <li> <a href="{{route('home')}}">Home</a> </li>
+            <li> <a href="{{route('prodotti')}}">Prodotti</a> </li>
+            <li> <a href="{{route('news')}}">News</a> </li>
+        </ul>
+    </div>
+</header>
