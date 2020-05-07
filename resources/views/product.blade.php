320ae4aea1006df8718ed5dbe5f4863525c3f1e2
frapgn <59645305+frapgn@users.noreply.github.com>
2020-05-07 22:20:12 +0200

    limiti gestiti

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -23,13 +23,21 @@
         <img src="{{$product['src-p']}}" alt="{{$product['titolo']}}">
         <p>{!!$product['descrizione']!!}</p>
     </div>
-    <a {{($id >= 2 && $id <= count($products) - 2 ? 'href="' . route('product.show', ($id - 1)) . '"' . ' class="products-navigation prev-prod"' : 'href="#"' . ' class="products-navigation prev-prod stop-navigation"')}}>
+    <a href="{{$id >= 1 && $id <= count($products) - 1 ? route('product.show', $id - 1) : '#'}}" class="products-navigation prev-prod">
+        <span class="{{$id < 1 ? 'hidden' : 'visible'}}"> < </span>
+        <span class="{{$id >= 1 ? 'hidden' : 'visible'}}"> x </span>
+    </a>
+
+    <a href="{{$id >= 0 && $id <= count($products) - 2 ? route('product.show', $id + 1) : '#'}}" class="products-navigation next-prod">
+        <span class="{{$id > count($products) - 2 ? 'hidden' : 'visible'}}"> > </span>
+        <span class="{{$id <= count($products) - 2 ? 'hidden' : 'visible'}}"> x </span>
+    </a>
+
+    {{-- <a href={{route('product.show', $id - 1)}} class="products-navigation prev-prod">
         <span> < </span>
-    </a>
-    @php
-        // dd(count($products));
-    @endphp
-    <a href="c" class="products-navigation next-prod">
+    </a> --}}
+
+    {{-- <a href={{route('product.show', $id + 1)}} class="products-navigation next-prod">
         <span> > </span>
-    </a>
+    </a> --}}
 @endsection
