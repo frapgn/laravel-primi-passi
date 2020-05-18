0f1d00e71d3a1f788e9fd2dcab1104d9a48d9e46
frapgn <59645305+frapgn@users.noreply.github.com>
2020-05-19 00:15:27 +0200

    done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Product;
 
 class ProductController extends Controller
 {
     public function products()
     {
-        return view('products');
+        // $products = new Product;
+        // $products->all();
+        $products = Product::all();
+        // dd($products);
+        // Modo 1 con array associativo
+        // return view('products', ['???' => $products]);
+
+        // modo 2 con la funzione compact()
+        return view('products', compact('products'));
     }
 
     public function show($id)
--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /*  Laravel capisce che la tabella alla quale vogliamo parlare
+    *   si chiama 'products' perchè guarda il nome della classe Product
+    *   e fa il plurale.
+    *   È come se avessimo già fatto per noi questo: */
+    public $table = 'products';
+
+    // Se vogliamo settare manualmente il nome della tabella possiamo fare così:
+    // protected $table = 'NOME_TABELLA';
+
+    // Se vogliamo settare manualmente il nome della primary key possiamo fare così:
+    // protected $primaryKey = 'NOME_PRIMARY_KEY'
 }
