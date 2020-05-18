f3f03366d23e7f1fb896c7a4642533fe6dfd5d93
frapgn <59645305+frapgn@users.noreply.github.com>
2020-05-18 17:26:32 +0200

    migrations

---
+++ database/migrations/2020_05_18_140720_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->text('src');
+            $table->text('src_h');
+            $table->text('src_p');
+            $table->string('title', 50);
+            $table->tinyInteger('cooking_time');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
